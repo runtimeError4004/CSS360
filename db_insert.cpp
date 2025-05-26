94efe0332d5dc19904d17eb4232232aad8932320
Caeden117 <caeden.s@outlook.com>
2025-05-25 18:14:21 -0700

    justin teaching me a lesson in simplicity

--- db_insert.cpp
+++ db_insert.cpp
@@ -96,31 +96,22 @@ void SQL_vaultReader(){
 */
 void SQL_attemptWriter(bool accessGranted){
     // Get current date and time
+    // This appears to be the easiest way to format Date and Time... thanks, C++!
     time_t now = time(0);
     tm* ltm = localtime(&now);
-
-    // This appears to be the easiest way to format Date and Time... thanks, C++!
     char date[11]; // YYYY-MM-DD
     char timeStr[9]; // HH:MM:SS
     strftime(date, sizeof(date), "%Y-%m-%d", ltm);
     strftime(timeStr, sizeof(timeStr), "%H:%M:%S", ltm);
 
-    // Prepare SQL statement (this seems to be better SQLite3 practice? im just Google searching this, man)
-    string sql = "INSERT INTO ACCESS_LOG (Valid, Date, Time) VALUES (?, ?, ?);";
-    sqlite3_stmt* stmt;
+    string query = "INSERT INTO ACCESS_LOG (Valid, Date, Time) VALUES ('" + string(accessGranted ? "1" : "0") + "', '" + date + "', '" + timeStr + "');";
 
-    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
-        sqlite3_bind_int(stmt, 1, accessGranted ? 1 : 0);
-        sqlite3_bind_text(stmt, 2, date, -1, SQLITE_TRANSIENT);
-        sqlite3_bind_text(stmt, 3, timeStr, -1, SQLITE_TRANSIENT);
+    SQL_QUERY = sqlite3_exec(db, query.c_str(), NULL, 0, NULL);
 
-        if (sqlite3_step(stmt) != SQLITE_DONE) {
-            cerr << "Failed to insert access log: " << sqlite3_errmsg(db) << endl;
-        }
-    } else {
-        cerr << "Failed to prepare insert statement: " << sqlite3_errmsg(db) << endl;
+    if (SQL_QUERY != SQLITE_OK) {
+        cerr << "Error executing WRITE statement: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
     }
-    sqlite3_finalize(stmt);
 }
 
 
@@ -129,31 +120,15 @@ void SQL_attemptWriter(bool accessGranted){
     
 */
 void SQL_attemptReader(){
-    string query = "SELECT ID, Valid, Date, Time FROM ACCESS_LOG;";
-    sqlite3_stmt* stmt;
+    // basically copy/paste of SQL_vaultReader but from a different table...
+    string query = "SELECT * FROM ACCESS_LOG;";
 
-    if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) != SQLITE_OK) {
-        cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
-        return;
+    SQL_QUERY = sqlite3_exec(db, query.c_str(), callback, (void*)"ACCESS_LOG", NULL); 
+
+    if (SQL_QUERY != SQLITE_OK) {
+        std::cerr << "Error executing READING statement: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
     }
-
-    cout << "Access Log Entries:\n";
-    while (sqlite3_step(stmt) == SQLITE_ROW) {
-        int id = sqlite3_column_int(stmt, 0);
-        int valid = sqlite3_column_int(stmt, 1);
-        const unsigned char* date = sqlite3_column_text(stmt, 2);
-        const unsigned char* time = sqlite3_column_text(stmt, 3);
-
-        cout << "ID: " << id
-             << ", Date: " << (date ? reinterpret_cast<const char*>(date) : "NULL")
-             << ", Time: " << (time ? reinterpret_cast<const char*>(time) : "NULL")
-             << ", Success: " << (valid == 1 ? "Yes" : "No") << endl;
-    }
-
-    sqlite3_finalize(stmt);
-
-    cout<<"\n/////function incomplete/////\n";
-
 }
 
 int SQL_vaultCounter(){
